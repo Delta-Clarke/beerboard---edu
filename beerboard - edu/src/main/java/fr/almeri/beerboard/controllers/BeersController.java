944bf1857c0da66d7399cac23415b552ff5ae383
Delta-Clarke <leo.daubord@live.fr>
2022-06-27 16:49:45 +0200

    1649

--- beerboard - edu/src/main/java/fr/almeri/beerboard/controllers/BeersController.java
+++ beerboard - edu/src/main/java/fr/almeri/beerboard/controllers/BeersController.java
@@ -1,13 +1,16 @@
 package fr.almeri.beerboard.controllers;
 
-import fr.almeri.beerboard.models.Biere;
-import fr.almeri.beerboard.models.BiereId;
+import fr.almeri.beerboard.models.*;
 import fr.almeri.beerboard.repositories.BiereRepository;
+import fr.almeri.beerboard.repositories.MarqueRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.ArrayList;
 
@@ -17,6 +20,8 @@ public class BeersController {
     @Autowired
     private BiereRepository biereRepository;
 
+    @Autowired
+    private MarqueRepository marqueRepository;
 
     @GetMapping("/beers")
     public String getListBeers(Model pModel){
@@ -36,5 +41,28 @@ public class BeersController {
         // Permet d'envoyer les attribut récupéré en bdd pour l'envoyer sur le fichier HTML
         pModel.addAttribute("biere", biere);
         return "see-beer";
+    }
+    @GetMapping("/add-beer")
+    public String setBrewery(Model pModel){
+        ArrayList<Marque> listMarqueFromDatabase = (ArrayList<Marque>) marqueRepository.findAll();
+        pModel.addAttribute("listMarque", listMarqueFromDatabase);
+        return "add-beer";
+    }
+    @PostMapping("/add-brewery")
+    public String addBeer(Model pModel, @ModelAttribute Biere biere, RedirectAttributes redirectAttributes){
+        // On vérifie si l'identifiant existe
+        if (biereRepository.existsById(marque.getNomMarque())){
+            // S'il existe, on renvoie un message d'erreur.
+            redirectAttributes.addFlashAttribute("messageErreur", "L'identifiant existe déjà.");
+            // On redirige vers le formulaire d'ajout
+            return "redirect:/add-brewery";
+        }else {
+            // Sinon On enregistre dans la BDD
+            biereRepository.save(biere);
+            // Envoie d'un message de succées
+            redirectAttributes.addFlashAttribute("messageSucces", "La brasserie a bien été enregistrée.");
+            // Redirige vers la liste des brasseries
+            return "redirect:/beers";
+        }
     }
 }
--- beerboard - edu/src/main/java/fr/almeri/beerboard/models/Biere.java
+++ beerboard - edu/src/main/java/fr/almeri/beerboard/models/Biere.java
@@ -43,6 +43,7 @@ public class Biere {
     //CONSTRUCTEUR
     public Biere() {}
 
+
     //GETTERS & SETTERS
     public Marque getMarque() {
         return marque;
